d10e429346fe35536271b3142951b98c8686e497
Richardos17 <116384806+Richardos17@users.noreply.github.com>
2023-03-04 15:18:45 +0100

    edf

--- dbc.js
+++ dbc.js
@@ -1,15 +1,22 @@
-var mysql = require('mysql');
-var con = mysql.createConnection({
-    host: "sv9.byethost9.org",
-    user: "gamezwir_data",
-    password: "2K75w5muSZLIlTh",
-    database: "gamezwir_data"
+var mysql = require('pg');
+var con = new Client({
+    host: "dpg-cg1l2do2qv25u2hk5u2g-a",
+    user: "data_b3ss_user",
+    password: "lXlR9IsZXtqtlqoLEgDj9djuT5rvBm6V",
+    database: "data_b3ss"
   });
+  con.connect((err) => {
+    if (err) {
+      console.error('connection error', err.stack)
+    } else {
+      console.log('connected')
+    }
+  })
 function query(sql, callback){
     
       con.query(sql, function (err, result) {
         if (err) throw err;
-        callback(result, con)
+        callback(result)
       });
       
 }
